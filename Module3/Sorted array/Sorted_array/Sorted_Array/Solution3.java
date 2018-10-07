d1dd28aa8475a53e5a8aa4e3f711b20de57e94dd
pavantadinada <pavan.tadinada1995@gmail.com>
2018-10-07 19:14:41 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- Module3/Sorted array/Sorted_array/Sorted_Array/Solution3.java
+++ Module3/Sorted array/Sorted_array/Sorted_Array/Solution3.java
@@ -23,12 +23,12 @@ class Sortedarray {
 			k++;
 		}
 		Arrays.sort(b);
-		System.out.println("Common elements in both the arrays in sorted order");
+		//System.out.println("Common elements in both the arrays in sorted order");
 		for(int y=0;y<K-1;y++)
 		{
 			if(b[y]==b[y+1])
 			{
-				System.out.print(b[y]+" ");
+				System.out.print(b[y]+",");
 			}
 		}
 		/*int k=0;
@@ -83,19 +83,19 @@ public class Solution3 {
 	{
 		int n,m,o = 0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of array1");
+		//System.out.println("Enter the size of array1");
 		n = sc.nextInt();
-		System.out.println("Enter the size of array2");
+		//System.out.println("Enter the size of array2");
 		m = sc.nextInt();
 		int a1[] = new int[n];
 		int a2[] = new int[m];
 		int b[] = new int[n+m];
-		System.out.println("Enter the elements in to the array1");
+		//System.out.println("Enter the elements in to the array1");
 		for(int i=0;i<n;i++)
 		{
 			a1[i] = sc.nextInt();
 		}
-		System.out.println("Enter the elements in to the array2");
+		//System.out.println("Enter the elements in to the array2");
 		for(int j=0;j<m;j++)
 		{
 			a2[j] = sc.nextInt();
